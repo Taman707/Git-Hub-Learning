eaf3320374bd21e2c7f38a857bdf4bfe8f49daea
Tamanpreet Singh <tamanpreetsingh76@gmail.com>
2025-03-01 00:38:15 +0530

    Self-Check

--- name.js
+++ name.js
@@ -1,1 +1,2 @@
 console.log('Tamanpreet Singh');
+console.log('May be one more trying will help me get a better understanding . I wam sitting from 10:45 and currently its 12:37 and I am ony 20 minutes into the video');
