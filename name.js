b3f550832e7d2a9ed022171b683a641e0ac4a364
Tamanpreet Singh <tamanpreetsingh76@gmail.com>
2025-03-02 11:07:47 +0530

    update-2-march

--- name.js
+++ name.js
@@ -1,2 +1,3 @@
 console.log('Tamanpreet Singh');
 console.log('May be one more trying will help me get a better understanding . I wam sitting from 10:45 and currently its 12:37 and I am ony 20 minutes into the video');
+console.log('Again I am at the video fromm 11 am');
